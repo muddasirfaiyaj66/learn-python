4ff793bc9f72c2d12912aab3e91eae72229f3667
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2025-03-15 14:41:19 +0600

    container complete

---
+++ containers.py
@@ -0,0 +1,28 @@
+# containers setup
+# a_tuple = (1,2,3,' a string')
+# a_list = [1,2,3,' a string',2]
+# a_set={1,2,5,8}
+# print(a_tuple)
+# print(a_list)
+# print(set(a_list))
+# print(list(set(a_list)))
+# a_list.append(5)
+# print(a_list)
+
+# print(a_set)
+
+
+a_dictionary = {'key':'value',123:[1,2,3]}
+
+# how to get values from a container
+user_list = ['Lisa','Bob', 'Alex', 'Anna','John']
+# print(user_list)
+# print(user_list[0])
+# print(user_list[0:3:1])
+# print(a_dictionary[123])
+
+#exercise list
+
+
+exercise_list = [1,2,3,4,5,6,7,8,9,10]
+print(exercise_list[8:2:-2])
